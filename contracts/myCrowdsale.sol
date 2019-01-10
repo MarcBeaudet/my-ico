f926b78eccc096bd3cf3a6fef125990c8117ef1a
MarcBeaudet <marc-antoine.beaudet@devinci.fr>
2019-01-10 11:25:35 +0100

    should deploy crowdsale

--- contracts/myCrowdsale.sol
+++ contracts/myCrowdsale.sol
@@ -2,21 +2,20 @@ pragma solidity ^0.4.24;
 
 import './myToken.sol';
 import 'zeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol';
-import 'zeppelin-solidity/contracts/crowdsale/validation/TimedCrowdsale.sol';
 import 'zeppelin-solidity/contracts/crowdsale/validation/WhitelistedCrowdsale.sol';
 import 'zeppelin-solidity/contracts/crowdsale/price/IncreasingPriceCrowdsale.sol';
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
-contract myCrowdsale is  TimedCrowdsale, MintedCrowdsale, WhitelistedCrowdsale, IncreasingPriceCrowdsale  {
+contract myCrowdsale is  MintedCrowdsale, WhitelistedCrowdsale, IncreasingPriceCrowdsale  {
 
   constructor
     (
        uint256 _openingTime,
        uint256 _closingTime,
        uint256 _rate,
-       address _wallet,
        uint256 _initialRate,
        uint256 _finalRate,
+       address _wallet,
        MintableToken _token
     )
     public
--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,5 +1,8 @@
 var myToken = artifacts.require("./myToken.sol");
+var myCrowdsale = artifacts.require("./myCrowdsale.sol")
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(myToken, "TokenYouNeed", "TYN", 8)
+  deployer.deploy(myToken, "TokenYouNeed", "TYN", 8).then(() => {
+    deployer.deploy(myCrowdsale, 1549756800, 1552176000, 50, 5, 5, "0x54e7f893392a2e88bb3a1eac6fc1e51deeb49eb7",  myToken.address)
+  })
 }
